5095d26c15a351ac95628ca495f53e3542b72158
geo <voltagnelly@gmail.com>
2023-09-22 17:09:49 -0300

    insert in bd

--- horas.py
+++ horas.py
@@ -1,19 +1,48 @@
 # App para recoger h trabajadas
 #importar libreria sqlite3.
+from datetime import datetime
 from os.path import isfile
 import sqlite3
 import os
 #conectar a la base de datos si no existe la creacion
-sqlite3.connect("bd/base_datos.db")
+coneccion=sqlite3.connect("bd/base_datos.db")
+#ruta de mi base de datos
 ruta = "bd/base_datos.db"
-
-
+#cursor para manipular mi base de datos
+cursor=coneccion.cursor()
 def existe_archivo(ruta):
   if os.path.isfile(ruta):
     print("existe el archivo")
   else:
     print("no existe el archivo")
 
+#existe_archivo(ruta)
 
-existe_archivo(ruta)
-#ahora creo
+#Funcion para crear la tabla
+def crear_tabla():
+  # Crear la tabla clases
+  cursor.execute('''
+      CREATE TABLE clases (
+          nombre TEXT,
+          hora_inicio TEXT,
+          hora_fin TEXT,
+          alumno TEXT
+      )
+  ''')
+#crear_tabla()
+#funcion para elimnar la tabla
+def eliminar_tabla(Tabla):
+  cursor.execute(f'DROP TABLE {Tabla}')
+#eliminar_tabla("clasesEJ")
+nombre="PHP"
+alumno="Damian"
+
+hora_inicio=str(datetime.now())
+hora_fin=str(datetime.now())
+
+cursor.execute(f'INSERT INTO clases(nombre, hora_inicio, hora_fin, alumno) VALUES(?, ?, ?, ?)', (nombre, hora_inicio, hora_fin, alumno))
+
+coneccion.commit()
+
+
+coneccion.close()
